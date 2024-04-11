d23f47ea4f899d5dc7052b641bbc33afc6516a29
Chizer77 <97499725+Chizer77@users.noreply.github.com>
2024-04-11 22:26:27 +0800

    feat: 添加简易运行计时器

--- Compiler.cpp
+++ Compiler.cpp
@@ -2,11 +2,22 @@
 #include "util/Init.h"
 #include "NfaTest.h"
 #include "DfaTest.h"
+#include <ctime>
 
+void log(void (*func)()) {
+    timespec start{}, end{};
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    (*func)();
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    double duration = ((double)(end.tv_nsec-start.tv_nsec) / ((double) 1e9) + (double)(end.tv_sec-start.tv_sec)) * 1000;
+    std::cout << "Completed in " << duration << "ms." << std::endl;
+    std::cout << "--------------------------" << std::endl;
+}
 
 int main() {
     std::cout << "hello world" << std::endl;
-
-    NfaTest::run();
-    DfaTest::run();
+    std::cout << "NfaTest runs..." << std::endl;
+    log(NfaTest::run);
+    std::cout << "DfaTest runs..." << std::endl;
+    log(DfaTest::run);
 }
