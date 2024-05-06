4caa8f5ffb8c5a23b608eb296a63c57ff1e0bce9
ayang-linux <3392406201@qq.com>
2024-05-07 07:20:02 +0800

    feat:消除左递归

--- src/frontend/LL.cpp
+++ src/frontend/LL.cpp
@@ -1,10 +1,140 @@
 #include "frontend/LL.h"
+#include "unordered_map"
+
+std::unordered_map<int, std::vector<Productions*>>* format(const CFG& cfg) {
+    auto *ans = new std::unordered_map<int, std::vector<Productions*>>;
+    for (const Productions& productions : cfg.products) {
+        std::vector<Productions*> productions_list;
+        std::vector<int> list;
+        for (int grammer : productions.grammar) {
+            if (grammer == CFG::UNION_ID) {
+                auto *productions_new = new Productions(productions.start, list);
+                productions_list.push_back(productions_new);
+                list.clear();
+            } else {
+                list.push_back(grammer);
+            }
+        }
+        auto *productions_new = new Productions(productions.start, list);
+        productions_list.push_back(productions_new);
+        ans->insert({productions_new->start, productions_list});
+    }
+    return ans;
+}
 
 
+Productions* reFormat(std::vector<Productions*>& productions_list) {
+    auto *list = new std::vector<int>();
+    for (int i = 0; i < productions_list.size(); i++) {
+        for (int garmmer : productions_list[i]->grammar) {
+            list->push_back(garmmer);
+            if (i != productions_list.size()-1) {
+                list->push_back(CFG::UNION_ID);
+            }
+        }
+    }
+    auto *ans = new Productions(productions_list[0]->start, *list);
+    return ans;
+}
 
+// TODO:free 内存
 CFG* LL::LeftRecurElimination(const CFG& cfg) {
-    CFG *opg = new CFG();
-    return opg;
+    CFG *opg1 = new CFG();
+    auto m_productions = format(cfg);
+
+    // 1. 消除间接左递归
+    //     * 如同 S -> Aa|b   A -> Ac|Sd
+    //     * 会产生  S ==> Aa ==> Sda
+    //     * 消除间接左递归变成
+    //     * S -> Aa|b
+    //     * A -> Ac|Aad|bd
+
+    std::unordered_set<Productions, Productions::ProductionsHasher> ans_1;
+    // 记录之前遍历过的 productions
+    auto *pre_productions = new std::unordered_map<int, std::vector<Productions*>>();
+    for (auto productions_pair = m_productions->begin(); productions_pair != m_productions->end(); ++productions_pair) {
+        auto entry_productions = new std::vector<Productions*>();
+        for (auto *production : productions_pair->second) {
+            int rightFirst = production->grammar[0];
+            auto it = pre_productions->find(rightFirst);
+            if (it == nullptr) {
+                // 没有间接递归
+                entry_productions->push_back(production);
+            } else {
+                auto pre_list = it->second;
+                for (auto pre : pre_list) {
+                    production->grammar.erase(production->grammar.begin());
+                    production->grammar.insert(production->grammar.begin(), pre->grammar.begin(), pre->grammar.end());
+                }
+            }
+        }
+        pre_productions->insert({productions_pair->first, *entry_productions});
+        ans_1.insert(*(reFormat(*entry_productions)));
+    }
+
+    opg1->nonter = cfg.nonter;
+    opg1->start = cfg.start;
+    opg1->ter = cfg.ter;
+    opg1->products = ans_1;
+
+    // 2. 消除直接左递归
+    CFG *opg2 = new CFG();
+    auto m_productions_2 = format(*opg2);
+
+    std::unordered_set<Productions, Productions::ProductionsHasher> ans_2;
+    for (auto productions_pair = m_productions_2->begin(); productions_pair != m_productions_2->end(); ++productions_pair) {
+        std::vector<Productions*> no_left_eliminate_productions;
+        std::vector<Productions*> left_eliminate_productions;
+        for  (Productions *productions : productions_pair->second) {
+            // 直接递归
+            if (productions->grammar[0] == productions->start) {
+                left_eliminate_productions.push_back(productions);
+            } else {
+                no_left_eliminate_productions.push_back(productions);
+            }
+        }
+        // 都不包含左递归
+        if (left_eliminate_productions.empty()) {
+            Productions *new_productions = reFormat(left_eliminate_productions);
+            ans_2.insert(*new_productions);
+        } else {
+           auto *res_productions = new std::vector<Productions*>();
+            // 就是将这种格式  P -> Pa|Pb|Pc|Pd|e|f|g|h
+            //     * 转成:
+            //     * P -> eP'|fP'|gP'|hP'
+            //     * p'-> aP'|bP'|cP'|dP'|ε
+            //
+            // 1.  // 将原来的 P -> e|f|g|h 变成    P -> eP'|fP'|gP'|hP'
+            int new_nonter = CFG::newId();
+            opg1->nonter.insert(new_nonter);
+            for (Productions *productions : no_left_eliminate_productions) {
+                productions->grammar.push_back(new_nonter);
+                res_productions->push_back(productions);
+
+            }
+            // 2. 将原来的 P -> Pa|Pb|Pc|Pd 变成 P’ -> aP'|bP'|cP'|dP'|ε
+            for (Productions *productions : left_eliminate_productions) {
+                // 删除左递归第一个
+                productions->grammar.erase(productions->grammar.begin());
+                // 插入新元素
+                productions->grammar.push_back(new_nonter);
+                auto *new_p = new Productions(new_nonter, productions->grammar);
+                res_productions->push_back(productions);
+            }
+            std::vector<int> empty_vector(CFG::EMPTY_ID);
+            auto *empty = new Productions(new_nonter, empty_vector);
+            res_productions->push_back(empty);
+
+            ans_2.insert(*(reFormat(*res_productions)));
+        }
+    }
+
+    opg2->nonter = opg1->nonter;
+    opg2->start = opg1->start;
+    opg2->ter = opg1->ter;
+    opg2->products = ans_2;
+
+    return opg2;
 }
 
 
